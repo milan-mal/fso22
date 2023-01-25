1cfed057c3b4ff0f73b15814e2849eaf848554e6
Milan M <milanmalenak@gmail.com>
2023-01-25 21:59:35 +0100

    3.2: Added /info that shows date and person count.

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    res.send(`<p>Phone book has info for ${persons.length} people</p><p>${new Date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
