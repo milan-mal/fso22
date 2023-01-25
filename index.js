8496d36c2754c77c479589de37b185b358876c22
Milan M <milanmalenak@gmail.com>
2023-01-25 22:06:36 +0100

    3.3: Returning of a specific person by id.

--- index.js
+++ index.js
@@ -31,12 +31,24 @@ app.get('/', (req, res) => {
     res.send('<h1>Congrats! You\'ve reached the server.</h1>')
 })
 
+app.get('/info', (req, res) => {
+    res.send(`<p>Phone book has info for ${persons.length} people</p><p>${new Date}</p>`)
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<p>Phone book has info for ${persons.length} people</p><p>${new Date}</p>`)
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.statusMessage = "Person with this id doesn't exist."
+        res.status(404).end()
+    }
 })
 
 const PORT = 3001
