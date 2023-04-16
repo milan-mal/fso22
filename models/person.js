c83db38b3d2b5d546bd949755371d999245764d6
Milan M <milanmalenak@gmail.com>
2023-04-16 19:15:46 +0200

    3.20*: Added validaton for a phone number.

--- models/person.js
+++ models/person.js
@@ -14,7 +14,17 @@ const phoneSchema = new mongoose.Schema({
         minLength: 3,
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: 9,
+        validate: {
+            validator: function(v) {
+                return /^(\d{2,3}-\d{5,})$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number.`
+          },
+        required: true,
+    },
 })
 
 phoneSchema.set('toJSON', {
